8ac5a8d000bc01f2ff86b49d083a78fc06fab813
leeduy1 <levanduy.gv2003@gmail.com>
2023-03-24 21:46:24 +0700

    feat: Ui UserSideNav

--- src/pages/User/Components/UserSideNav/UserSideNav.tsx
+++ src/pages/User/Components/UserSideNav/UserSideNav.tsx
@@ -1,3 +1,69 @@
+import { Link } from 'react-router-dom'
+import path from 'src/constants/path'
+
 export default function userSideNav() {
-  return <div>userSideNav</div>
+  return (
+    <div>
+      <div className='flex items-center border-b border-b-gray-200 py-4'>
+        <Link to={path.profile} className='h-12 w-12 flex-shrink-0 overflow-hidden rounded-full border border-black/10'>
+          <img
+            src='https://scontent.fdad3-1.fna.fbcdn.net/v/t39.30808-6/337050445_916604416151032_994900670465870285_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=5SX-9sipUM8AX8Ey-qS&_nc_ht=scontent.fdad3-1.fna&oh=00_AfB8hXw6EA77kOyMPSX7dGKl2rQURPEOZq6Xn-0Fg-Rdwg&oe=642300CB'
+            alt=''
+            className='h-full w-full object-cover'
+          />
+        </Link>
+        <div className='flex-grow pl-4'>
+          <div className='mb-1 truncate font-semibold text-gray-600'>cdthanh</div>
+          <Link to={path.profile} className='flex items-center capitalize text-gray-500'>
+            <svg
+              width={12}
+              height={12}
+              viewBox='0 0 12 12'
+              xmlns='http://www.w3.org/2000/svg'
+              style={{ marginRight: 4 }}
+            >
+              <path
+                d='M8.54 0L6.987 1.56l3.46 3.48L12 3.48M0 8.52l.073 3.428L3.46 12l6.21-6.18-3.46-3.48'
+                fill='#9B9B9B'
+                fillRule='evenodd'
+              />
+            </svg>
+            Sửa hồ sơ
+          </Link>
+        </div>
+      </div>
+      <div className='mt-7'>
+        <Link to={path.profile} className='flex items-center capitalize text-orange transition-colors'>
+          <div className=' mr-3 h-[22px] w-[22px]'>
+            <img
+              src='https://down-vn.img.susercontent.com/file/ba61750a46794d8847c3f463c5e71cc4'
+              alt=''
+              className='h-full w-full'
+            />
+          </div>
+          Tài khoản của tôi
+        </Link>
+        <Link to={path.changePassword} className='mt-4 flex items-center capitalize text-gray-600 transition-colors'>
+          <div className=' mr-3 h-[22px] w-[22px]'>
+            <img
+              src='https://down-vn.img.susercontent.com/file/ba61750a46794d8847c3f463c5e71cc4'
+              alt=''
+              className='h-full w-full'
+            />
+          </div>
+          Đổi mật khẩu
+        </Link>
+        <Link to={path.historyPurchase} className='mt-4 flex items-center capitalize text-gray-600 transition-colors'>
+          <div className=' mr-3 h-[22px] w-[22px]'>
+            <img
+              src='https://down-vn.img.susercontent.com/file/f0049e9df4e536bc3e7f140d071e9078'
+              alt=''
+              className='h-full w-full'
+            />
+          </div>
+          Đơn mua
+        </Link>
+      </div>
+    </div>
+  )
 }
